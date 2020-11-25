dcb0555f444899e0051bc83190a40350b907e113
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-24 16:24:21 -0800

    Queries 4

---
+++ 4_queries/name_of_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohort = 'JUL02'
+ORDER BY teacher;
+
